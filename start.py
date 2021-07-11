960c94d022ca33e9e0610c65bfc23db841509ad4
AVi <saviral425@gmail.com>
2021-07-11 19:55:04 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import time, rpc
 from time import mktime
 
 print("Starting Discord RPC.")
-client_id = '732437600622739538'
+client_id = 'Your Client ID'
 RPC_Discord = rpc.DiscordIpcClient.for_platform(client_id)
 print("RPC connection successful.")
 
@@ -10,14 +10,16 @@ time.sleep(5)
 start_time = mktime(time.localtime())
 while True:
     activity = {
-            "state": "Dream-Animation",
-            "details": "Developing React-APP ",
+            "state": "Your state",
+            "details": "Your details",
             "timestamps": {
                 "start": start_time
             },
             "assets": {
-                "large_text": "Dream-Animation",
-                "large_image": "untitled-1"
+                "large_text": "Your Text",
+                "large_image": "Your Image on Discord Developer portal"
+                "small_text": "Your Text",
+                "small_image": "Your Image on Discord Developer portal"
             }
         }
     RPC_Discord.set_activity(activity)
